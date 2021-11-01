9cd9930afbdf991e1ddfdf9e6bc7eae88533dbbe
Paulo Dehon <paulodehon@hotmail.com>
2021-11-01 13:00:34 -0300

    transformando arquivos js em jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
 import Footer from './components/Footer/Footer';
 
+
 function App() {
   return (
     <>
